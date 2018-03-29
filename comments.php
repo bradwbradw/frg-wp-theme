12e9d848efe2aee301966de3f90a4899b4b5d432
bradwbradw <bradwbradw@xyz.gs>
2018-03-29 14:35:59 -0700

    remove comments and tags from post page

---
+++ comments.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * The template for displaying Comments.
+ *
+ * no comments!
+ *
+ * @package Neubau
+ * @since Neubau 1.0
+ * @version 1.0
+ */
+
+if ( post_password_required() ) {
+	return;
+}
+?>
