3e3cd449adef088e247015b9e885deee199f26a0
bradwbradw <bradwbradw@xyz.gs>
2018-04-05 13:53:54 -0700

    search bar

---
+++ searchform.php
@@ -0,0 +1,8 @@
+<li id="search">
+    <form class="searchform" method="get" action="/index.php">
+      <div>
+         <input type="text" name="s" id="s" size="15" placeholder="Search" />
+         <!--<input type="submit" value="Go" /> -->
+      </div>
+     </form>
+</li>
